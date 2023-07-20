d4e4c552239bf8a5b81498dee81b6a37e2a3b2f9
AMIT KUMAR <amuzit0801@hotmail.com>
2023-07-20 19:51:56 +0530

    created basic folder structure and datacontext

---
+++ REST-API-SRC/Services/Catalog/Catalog.API/DataContext/ICatalogContext.cs
@@ -0,0 +1,11 @@
+﻿using Catalog.API.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.API.DataContext
+{
+    public interface ICatalogContext
+    {
+        IMongoCollection<Product> Products { get; }
+
+    }
+}
