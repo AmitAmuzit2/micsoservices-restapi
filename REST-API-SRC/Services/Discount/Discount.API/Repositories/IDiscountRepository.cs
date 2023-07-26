5ff29d4110b6b197fca2ff1079a8f148925a82d3
Amit <amuzit@gmail.com>
2023-07-26 12:44:43 +0530

    Discount.Api microservice with PostgreSQL

---
+++ REST-API-SRC/Services/Discount/Discount.API/Repositories/IDiscountRepository.cs
@@ -0,0 +1,15 @@
+﻿using Discount.API.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Discount.API.Repositories
+{
+    public interface IDiscountRepository
+    {
+        Task<Coupon> GetDiscount(string productName);
+        Task<IEnumerable<Coupon>> GetAllCoupons();
+        Task<bool> CreateDiscount(Coupon coupon);
+        Task<bool> UpdateDiscount(Coupon coupon);
+        Task<bool> DeleteDiscount(string productName);
+    }
+}
