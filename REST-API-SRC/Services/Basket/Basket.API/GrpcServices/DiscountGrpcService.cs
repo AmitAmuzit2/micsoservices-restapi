cc3e37f58cfee15dbc2e02fce5d4693179decf7e
Amit <amuzit@gmail.com>
2023-07-27 19:42:46 +0530

    discountgrpcservice implementation

---
+++ REST-API-SRC/Services/Basket/Basket.API/GrpcServices/DiscountGrpcService.cs
@@ -0,0 +1,21 @@
+﻿using Dicount.Grpc.Protos;
+using System;
+using System.Threading.Tasks;
+
+namespace Basket.API.GrpcServices
+{
+    public class DiscountGrpcService
+    {
+        public readonly DicountProtoService.DicountProtoServiceClient _discountProtoService;
+
+        public DiscountGrpcService(DicountProtoService.DicountProtoServiceClient discountProtoService)
+        {
+            _discountProtoService = discountProtoService ?? throw new ArgumentNullException(nameof(discountProtoService));
+        }
+        public async Task<CouponModel> GetDiscount(string productName)
+        {
+            var discountRequest = new GetDiscountRequest { ProductName = productName };
+            return await _discountProtoService.GetDiscountAsync(discountRequest);
+        }
+    }
+}
