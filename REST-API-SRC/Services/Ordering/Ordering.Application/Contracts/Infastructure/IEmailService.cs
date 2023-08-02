65b2d0c611cd42e80569cbe66dbaf115ff27cef8
Amit <amuzit@gmail.com>
2023-08-02 18:22:14 +0530

    ordering microservice with clean architecture and CQRS pattern

---
+++ REST-API-SRC/Services/Ordering/Ordering.Application/Contracts/Infastructure/IEmailService.cs
@@ -0,0 +1,10 @@
+﻿using Ordering.Application.Modals;
+using System.Threading.Tasks;
+
+namespace Ordering.Application.Contracts.Infastructure
+{
+    public interface IEmailService
+    {
+        Task<bool> SendEmail(Email email);
+    }
+}
